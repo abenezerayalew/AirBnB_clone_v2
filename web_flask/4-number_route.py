e8c6e15158d80f7f58f1876148c152f9537c35df
abenezerayalew <aben.man.unity@gmail.com>
2023-04-24 01:13:54 +0300

    4task

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -30,7 +30,7 @@ def pythoniscool(text='is cool'):
 @app.route('/number/<int:n>', strict_slashes=False)
 def imanumber(n):
     """display “n is a number” only if n is an integer"""
-    return "{:d} i a number".format(n)
+    return "{:d} is a number".format(n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
