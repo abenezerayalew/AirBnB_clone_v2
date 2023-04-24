7725f011b99aa1ff2f94e06afd688034df6873ef
abenezerayalew <aben.man.unity@gmail.com>
2023-04-24 15:58:41 +0300

    0task

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-script that starts a Flask web application
+starts a Flask Web application
 """
 
 from flask import Flask
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """return Hello HBNB!"""
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000' )
+    app.run(host='0.0.0.0', port='5000')
