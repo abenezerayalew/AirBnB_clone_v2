ccdbd1b9bf939725cee3086d0fcb88fdebf68e3b
abenezer_ayalew <44866028+abenezerayalew@users.noreply.github.com>
2023-04-26 15:24:15 +0300

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,29 +1,39 @@
-#!/usr/bin/python3
-"""
-script that starts a Flask web application
-"""
-
-from flask import Flask
-app = Flask(__name__)
-
-
-@app.route('/', strict_slashes=False)
-def index():
-    """return Hello HBNB!"""
-    return 'Hello HBNB!'
+#!/usr/bin/env python3
+""" Function that compress a folder """
+from datetime import datetime
+from fabric.api import *
+import shlex
+import os
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """return HBNB"""
-    return 'HBNB'
+env.hosts = ['44.211.97.124', '34.228.52.136']
+env.user = "ubuntu"    
+def do_deploy(archive_path):
+    """ Deploys """
+    if not os.path.exists(archive_path):
+        return False
+    try:
 
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
 
-@app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
-    return 'C ' + text.replace('_', ' ')
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
+ 
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(name)
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(releases_path))
+        print("New version deployed!")
+        return True
+    except:
+        return False
