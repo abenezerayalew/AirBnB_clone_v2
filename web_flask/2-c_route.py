d996b3c0ce584e89659f8794e0a5fd7b7f6bac38
abenezer_ayalew <44866028+abenezerayalew@users.noreply.github.com>
2023-04-26 15:33:31 +0300

    update

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-script that starts a Flask web application
+starts a Flask Web application
 """
 
 from flask import Flask
@@ -9,13 +9,13 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """return Hello HBNB!"""
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """return HBNB"""
+    """returns HBNB"""
     return 'HBNB'
 
 
@@ -23,7 +23,6 @@ def hbnb():
 def cisfun(text):
     """display “C ” followed by the value of the text variable"""
     return 'C ' + text.replace('_', ' ')
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
